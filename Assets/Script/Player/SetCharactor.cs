ef97f6ec82f49d95c0dfd1f62df6e7196396f752
Shouki Imai <ukiru77@gmail.com>
2019-08-23 14:46:57 +0900

    選択されたキャラクターを表示できるようにした(2つ)

---
+++ Assets/Script/Player/SetCharactor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCharactor : MonoBehaviour {
+	int charactorNum;
+	[SerializeField]
+	private List<GameObject> charactorList;
+	//スタート関数の前に呼び出される
+	void Awake(){
+		charactorNum=PlayerPrefs.GetInt("Charactor",0);
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log(charactorNum);
+		Instantiate(charactorList[charactorNum-1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
