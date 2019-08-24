0f6834ad95d72c6136479f3d4d7d0fdc5df2fd9b
Shouki Imai <ukiru77@gmail.com>
2019-08-24 14:38:59 +0900

    勝敗をジャッジできるようにした

---
+++ Assets/Script/Collider/Judge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Judge : MonoBehaviour
+{
+    private bool IsJudged=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit2D(Collider2D col){
+        if(!IsJudged){
+            Debug.Log(col+"is lose");
+            IsJudged=true;
+        }
+    }
+}
