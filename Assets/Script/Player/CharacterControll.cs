0d38aa5a8438791f1a18bc36b15097914c354946
Shouki Imai <ukiru77@gmail.com>
2019-09-24 17:32:30 +0900

    接触時のエフェクトの追加

--- Assets/Script/Player/CharacterControll.cs
+++ Assets/Script/Player/CharacterControll.cs
@@ -14,6 +14,8 @@ public class CharacterControll : MonoBehaviour {
     private ImtStateMachine<CharacterControll> stateMachine;
     private float CharactorStopThreshold=1f;
     private GameObject enemy;
+    [SerializeField]
+    private GameObject hitEffect;
     private enum StateEventID{
         Idle,
         Active,
@@ -112,5 +114,13 @@ public class CharacterControll : MonoBehaviour {
             gazeLength = 0;
         shotGaze.value = gazeLength;
         speed = gazeLength * 500f + 100f;
+    }
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag=="Enemy"){
+            foreach(ContactPoint2D point in col.contacts){
+                GameObject effect = Instantiate (hitEffect) as GameObject;
+                effect.transform.position = (Vector3)point.point;
+            }
+        }
     }
 }
