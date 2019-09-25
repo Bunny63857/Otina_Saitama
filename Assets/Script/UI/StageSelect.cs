b49685fd3f6572f95b94db7584878f9c8fdcefb7
Shouki Imai <ukiru77@gmail.com>
2019-09-25 12:34:56 +0900

    Add システムのクリック音

--- Assets/Script/UI/StageSelect.cs
+++ Assets/Script/UI/StageSelect.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 public class StageSelect : MonoBehaviour {
 	[SerializeField]
 	int stageNum;
+	private AudioSource sound;
 	// Use this for initialization
 	void Start () {
-		
+		sound=GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +17,11 @@ public class StageSelect : MonoBehaviour {
 	}
 	public void OnSelected(){
 		PlayerPrefs.SetInt("Stage",stageNum);
+		sound.PlayOneShot(sound.clip);
+		StartCoroutine(ChangeScene());
+	}
+	IEnumerator ChangeScene(){
+		yield return new WaitForSeconds(1.0f);
 		SceneManager.LoadScene("MainScene");
 	}
 }
